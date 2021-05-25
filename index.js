8b9011e7727603cc56ec6ab08d38874b8baca91d
ethanejohnsons <ethan@ejohnsons.com>
2021-05-24 23:31:13 -0500

    Set up command structure

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+// Initialize the client
+const client = new Discord.Client();
+
+// Gather up the commands
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+// Login to Discord
+client.login(token);
+
+// On any message received
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was a problem executing that command.');
+    }
+});
